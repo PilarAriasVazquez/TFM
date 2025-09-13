345ddce3a93d2bcb3adc0be24ec209cd17c44a85
juanfranHR <juanfranherrodri@gmail.com>
2025-09-13 18:52:54 +0200

    añadiendo info de la instalacion por pantalla

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
 python3 -m venv .venv
+echo "Activando el entorno virtual"
+source .venv/bin/activate
+
+echo "Instalando dependencias desde requirements.txt. Este proceso se puede alargar unos minutos"
 pip install -r requirements.txt
+
+echo "Iniciando la aplicación de Streamlit"
 streamlit run streamlit_app.py
